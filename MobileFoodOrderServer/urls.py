b610c27dbefcee73d25f7af4b0524c4130642100
浮世絵空事 <UkiyoESoragoto@gmail.com>
2015-03-28 13:05:45 +0800

    new

--- MobileFoodOrderServer/urls.py
+++ MobileFoodOrderServer/urls.py
@@ -1,10 +1,14 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+from order import views
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'MobileFoodOrderServer.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^$', views.IndexView.as_view(), name="home"),
 )
--- order/views.py
+++ order/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.views import generic
 
 # Create your views here.
+
+
+class IndexView(generic.View):
+    template_name = 'order/templates/index.html'
+
+    def get(self, request):
+
+        temp_text = "Hello"
+        context = {
+            'temp_text': temp_text,
+        }
+        return render(
+            request,
+            self.template_name,
+            context
+        )
